d9cb9e7a77c1bc5de634d4d681bb7bde9f6a0043
Yagyandatta Murmu <60855589+yagyandatta@users.noreply.github.com>
2021-11-03 16:51:15 +0530

    Update docker_setup.sh

--- docker_setup.sh
+++ docker_setup.sh
@@ -1,10 +1,11 @@
 # creating Docker repo for docker instalation
-sudo cat <<EOF > /etc/yum.repos.d/docker.repo
+sudo cat << EOF > /etc/yum.repos.d/docker.repo
 [docker]
 name=docker
 baseurl=https://download.docker.com/linux/centos/7/x86_64/stable/
 gpgcheck=0
 EOF
+
 sudo yum install docker-ce --nobest -y
 
 sudo systemctl restart docker
