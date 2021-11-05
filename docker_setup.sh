bbe438758cb23e80f92f76facb68113d2e198ec0
Yagyandatta Murmu <60855589+yagyandatta@users.noreply.github.com>
2021-11-05 16:38:53 +0530

    Update docker_setup.sh

--- docker_setup.sh
+++ docker_setup.sh
@@ -13,7 +13,7 @@ sudo systemctl restart docker
 sudo docker ps
 
 sudo docker run -dit --name myos1 -p 8085:80 centos:7
-sudo docker exec -i myos1  yum install nginx  -y
-sudo docker exec -i myos1  yum install net-tools  -y
-sudo docker cp  /root/jenkins/index.html  myos1:/usr/share/nginx/html/
-sudo docker exec -i myos1  /usr/sbin/nginx
+sudo docker exec -i myos2  yum install nginx  -y
+sudo docker exec -i myos2  yum install net-tools  -y
+sudo docker cp  /root/jenkins/index.html  myos2:/usr/share/nginx/html/
+sudo docker exec -i myos2  /usr/sbin/nginx
