c81d0d31e60563abc92e2e1126a75cc691401778
Yagyandatta Murmu <60855589+yagyandatta@users.noreply.github.com>
2021-11-05 22:23:13 +0530

    Update docker_setup.sh

--- docker_setup.sh
+++ docker_setup.sh
@@ -12,8 +12,8 @@ sudo systemctl restart docker
 
 sudo docker ps
 
-sudo docker run -dit --name myos2 -p 8085:80 centos:7
-sudo docker exec -i myos2  yum install httpd  -y
-sudo docker exec -i myos2  yum install net-tools  -y
-sudo docker cp  /root/jenkins/index.html  myos2:/var/www/html/
-sudo docker exec -i myos2  /usr/sbin/httpd
+sudo docker run -dit --name myos3 -p 8085:80 centos:7
+sudo docker exec -i myos3  yum install httpd  -y
+sudo docker exec -i myos3  yum install net-tools  -y
+sudo docker cp  /root/jenkins/index.html  myos3:/var/www/html/
+sudo docker exec -i myos3  /usr/sbin/httpd
